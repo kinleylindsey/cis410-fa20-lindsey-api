373c260818f8647497a841b60a0ab090b07f8d3c
kinleylindsey <kinley@rams.colostate.edu>
2020-11-05 21:04:34 -0700

    11/5 initial commit

---
+++ dbConnectExec.js
@@ -0,0 +1,21 @@
+const sql = require('mssql')
+const lindseyConfig = require('./config.js')
+
+const config = {
+    user: lindseyConfig.DB.user,
+    password: lindseyConfig.DB.password,
+    server: lindseyConfig.DB.server,
+    database: lindseyConfig.DB.database,
+}
+
+
+async function executeQuery(aQuery){
+    var connection = await sql.connect(config)
+    var result = await connection.query(aQuery)
+
+    return result.recordset
+}  
+
+executeQuery()
+
+module.exports = {executeQuery: executeQuery}
